9d780ece3429d37a7085b41f24ee82cac9582194
LIU-Xnd <lqxnds@foxmail.com>
2024-12-16 14:53:35 +0800

    v0.9.3 - (Re-commit) Added README. Fixed bugs: 1. added .normalized(); 2. garbage filtrations collection; 3. missing negative sign in rbf probs; 4. miss-spelling

--- pytacs/__init__.py
+++ pytacs/__init__.py
@@ -1,19 +1,20 @@
-# pytacs: python Topological Automatic Cell Segmentation -
+# pytacs: Python Topological Automatic Cell Segmentation -
 #  an improved version of TopACT (https://gitlab.com/kfbenjamin/topact)
 #  implementation.
 
+# Author: Liu X., 2024
 
 # TODO:
 # - Only use overlapped genes -> wrapped in a new class [v]
 # - Add negative control (simulated) training samples [v]
-# - Abstract LocalClassifier []
-#   + Define SVC and MLP as subclasses [] 
-# - Add README []
+# - Abstract LocalClassifier [x] - SVM works fine so far, after debugging
+#   + Define SVC and MLP as subclasses [x] 
+# - Add README [v]
 # - Add requirements.txt []
 # - Improve documentations []
 
 
-__version__ = '0.9.1'
+__version__ = '0.9.3'
 
 from .classifier import LocalClassifier
 from .spatial import SpatialHandler

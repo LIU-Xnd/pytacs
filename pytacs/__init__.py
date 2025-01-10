8f1ee738825e2a5825c4b4129a75bb0bfe4b4d2c
LIU-Xnd <lqxnds@foxmail.com>
2025-01-10 23:07:50 +0800

    v1.1.0 (beta) - Plan for TACSIT: Topological Automatic Cell Segmentation using Imaging data and Transcriptomic data.

--- pytacs/__init__.py
+++ pytacs/__init__.py
@@ -4,26 +4,31 @@
 
 # Author: Liu X., 2024.12
 
-# Updates v1.0.5:
-# - [v] Add param standardize_PCs (bool) to QProximityClassifier to scale
-#  different PC dimensions for proximity balls to work better.
-# - [v] Add param n_spots_add_per_step to SpatialHandler to speed up.
+# Updates v1.1.0:
+# Plan:
+# - Try to merge imaging data for a
+# image-based local classifier (CellPose + TopACT)
+#   + Find ways to generate confidence from imaging
+#    Maybe the highest IOU?
+#   + Estimate cell size (n_spots) from imaging
+#   + Python implemented CellPose API
+#   + Strategy of adding spots: learning from imaging
 
 # Future:
-# - Add params normalize, log1p, on_PCs, n_PCs to classifier.SVM.
-# - Write __repr__ for _LocalClassifier and its child classes.
+
 # - Improve run_getSingleCellAnnData()
 #    + Add: Coordinate remapping
 #    + Add: Shape smoothing (if a spot is surrounded by spots of the same
 #    class, then make it of that class, too)
 # - Improve documentations
-# - Image based local classifier (CellPose + TopACT)
 # - The spatial coordinates by convention should have been saved in `.obsm`
 #  because `'x'` and `'y'` are related. But in this tool they are expected to be
 #  put separately as columns in `.obs`. This might be a break of convention that
 #  needs addressing in the future.
+# - Add params normalize, log1p, on_PCs, n_PCs to classifier.SVM.
+# - Write __repr__ for _LocalClassifier and its child classes.
 
-__version__ = "1.0.5"
+__version__ = "1.1.0"
 
 from .data import AnnDataPreparer
 from .classifier import SVM, GaussianNaiveBayes, QProximityClassifier

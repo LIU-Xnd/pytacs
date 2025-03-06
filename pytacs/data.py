14ea49e5c201d5443ef7877cbe32f0d9f726fcc8
LIU-Xnd <lqxnds@foxmail.com>
2025-03-06 11:14:07 +0800

    v1.2.1 (dev) - AnnDataPreparer: Allow empty load

--- pytacs/data.py
+++ pytacs/data.py
@@ -1,5 +1,6 @@
 from scanpy import AnnData as _AnnData
-import scanpy as _sc
+
+# import scanpy as _sc
 import pandas as _pd
 import numpy as _np
 from scipy.sparse import csr_matrix as _csr_matrix
@@ -34,8 +35,8 @@ class AnnDataPreparer:
 
     def __init__(
         self,
-        sn_adata: _AnnData,
-        sp_adata: _AnnData,
+        sn_adata: _AnnData | None = None,
+        sp_adata: _AnnData | None = None,
         sn_colname_celltype: str = "cell_type",
         sp_colnames_x_and_y: tuple[str, str] = ("x", "y"),
         overlapped_genes_warning: int = 10,
@@ -52,57 +53,57 @@ class AnnDataPreparer:
             sn_adata (_AnnData): snRNA-seq (or scRNA-seq) AnnData (scanpy object)
             sp_adata (_AnnData): spatial transcriptomic AnnData (scanpy object)
         """
-        # Checklist:
-        # - sn_adata:
-        #   + .X.shape[0] == len(.obs)
-        #   + .X.shape[1] == len(.var)
-        #   + .obs['cell_type']
-        #   + .obs.index must be 0, 1, ...
-        # - sp_adata:
-        #   + shape[0], [1] ...
-        #   + .obs[['x','y']]
-        #   + .obs.index must be 0, 1, ...
-        # - overlapped genes > 0
-        #   + if <= 10: print a warning
-
-        assert type(sn_adata) is _AnnData
-        assert type(sp_adata) is _AnnData
-        sn_adata_copy = sn_adata.copy()
-        sp_adata_copy = sp_adata.copy()
+        # Checklist
+        assert isinstance(sn_adata, _AnnData) or isinstance(sp_adata, _AnnData)
+        sn_adata_copy: _AnnData | _Undefined = _UNDEFINED
+        sp_adata_copy: _AnnData | _Undefined = _UNDEFINED
+        if sn_adata is not None:
+            sn_adata_copy = sn_adata.copy()
+        if sp_adata is not None:
+            sp_adata_copy = sp_adata.copy()
 
         for adata_i in (sn_adata_copy, sp_adata_copy):
+            if not isinstance(adata_i, _AnnData):
+                continue
             assert adata_i.X.shape[0] == len(adata_i.obs.index)
             assert adata_i.X.shape[1] == len(adata_i.var.index)
             _save_and_tidy_index(adata_i)
-            if not (type(adata_i.X) is _csr_matrix):
+            if not isinstance(adata_i.X, _csr_matrix):
                 adata_i.X = _csr_matrix(adata_i.X)
-        assert sn_colname_celltype in sn_adata.obs.columns
-        if sn_colname_celltype != "cell_type":
-            sn_adata_copy.obs["cell_type"] = sn_adata_copy.obs[
-                sn_colname_celltype
-            ].copy()
-            del sn_adata_copy.obs[sn_colname_celltype]
-            if (sn_colname_celltype + '_colors') in sn_adata_copy.uns.keys():
-                sn_adata_copy.uns['cell_type_colors'] = sn_adata_copy\
-                    .uns[sn_colname_celltype + '_colors']\
-                        .copy()
-
-        assert len(sp_colnames_x_and_y) == 2
-        sp_names_standard = ("x", "y")
-        for i_name, sp_name in enumerate(sp_colnames_x_and_y):
-            assert sp_name in sp_adata.obs.columns
-            if sp_name != sp_names_standard[i_name]:
-                sp_adata_copy.obs[sp_names_standard[i_name]] = sp_adata_copy.obs[
-                    sp_name
+        if isinstance(sn_adata_copy, _AnnData):
+            assert sn_colname_celltype in sn_adata.obs.columns
+            if sn_colname_celltype != "cell_type":
+                sn_adata_copy.obs["cell_type"] = sn_adata_copy.obs[
+                    sn_colname_celltype
                 ].copy()
-                del sp_adata_copy.obs[sp_name]
+                del sn_adata_copy.obs[sn_colname_celltype]
+                if (sn_colname_celltype + "_colors") in sn_adata_copy.uns.keys():
+                    sn_adata_copy.uns["cell_type_colors"] = sn_adata_copy.uns[
+                        sn_colname_celltype + "_colors"
+                    ].copy()
 
-        overlapped_genes: list[str] = list(set(sp_adata_copy.var.index) & set(
-            sn_adata_copy.var.index))
-        if len(overlapped_genes) <= overlapped_genes_warning:
-            print(f"Warning: Overlapped genes of two datasets too few!")
+        if isinstance(sp_adata_copy, _AnnData):
+            assert len(sp_colnames_x_and_y) == 2
+            sp_names_standard = ("x", "y")
+            for i_name, sp_name in enumerate(sp_colnames_x_and_y):
+                assert sp_name in sp_adata.obs.columns
+                if sp_name != sp_names_standard[i_name]:
+                    sp_adata_copy.obs[sp_names_standard[i_name]] = sp_adata_copy.obs[
+                        sp_name
+                    ].copy()
+                    del sp_adata_copy.obs[sp_name]
 
-        self.sn_adata: _AnnData = sn_adata_copy[:, overlapped_genes].copy()
+        if isinstance(sn_adata, _AnnData) and isinstance(sp_adata, _AnnData):
+            overlapped_genes: list[str] = list(
+                set(sp_adata_copy.var.index) & set(sn_adata_copy.var.index)
+            )
+            if len(overlapped_genes) <= overlapped_genes_warning:
+                print(f"Warning: Overlapped genes of two datasets too few!")
+
+        if isinstance(sn_adata, _AnnData) and isinstance(sp_adata, _AnnData):
+            self.sn_adata: _AnnData = sn_adata_copy[:, overlapped_genes].copy()
+        else:
+            self.sn_adata: _AnnData = sn_adata_copy
         self.sp_adata: _AnnData = sp_adata_copy
         self.__sn_adata_withNegativeControl: _AnnData | _Undefined = _UNDEFINED
         self.__normalized: bool = False
@@ -146,8 +147,7 @@ class AnnDataPreparer:
         n_new = int(_np.ceil(n_old * folds_newToOld))
         n_samplingFrom = int(_np.ceil(ratio_samplingFrom * n_old))
         X_old: _np.ndarray = self.sn_adata.X.toarray()
-        X_extra: _np.ndarray = _np.zeros(
-            shape=(n_new, X_old.shape[1]), dtype=int)
+        X_extra: _np.ndarray = _np.zeros(shape=(n_new, X_old.shape[1]), dtype=int)
         for i_gene in range(X_old.shape[1]):
             # Select the least n_samplingFrom expressed samples
             lowly_expressed = _np.sort(X_old[:, i_gene])[:n_samplingFrom].max()
@@ -173,8 +173,7 @@ class AnnDataPreparer:
                     [
                         self.sn_adata.obs["cell_type"],
                         _pd.Series(
-                            ["__NegativeControl" for _ in range(
-                                X_extra.shape[0])]
+                            ["__NegativeControl" for _ in range(X_extra.shape[0])]
                         ),
                     ]
                 ).values,
